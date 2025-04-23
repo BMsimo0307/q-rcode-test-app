d369b16f81cef71560626c1d574ecaa2cbf3932b
Benmoumen Mohammed <BMsimo0307@hotmail.com>
2025-04-23 12:43:55 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'QRcodeTestApp',
+  webDir: 'dist'
+};
+
+export default config;
